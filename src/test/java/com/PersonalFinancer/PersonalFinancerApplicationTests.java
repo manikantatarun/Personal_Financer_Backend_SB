658565715f5d9c9035f60e7d178d7766e0af4d77
Mani <b.tarun1@tcs.com>
2024-05-27 19:15:52 +0530

    Created project

---
+++ src/test/java/com/PersonalFinancer/PersonalFinancerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PersonalFinancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinancerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
